28616108125d6a1fdb56b3c742473bc27b331a3f
Finn von Friesland <finn.von.friesland@googlemail.com>
2014-02-08 13:47:34 +0100

    search on serch result page is now working

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -7,9 +7,57 @@ class ShirtsController < ApplicationController
     @shirts = Shirt.all
   end
 	
-	def search
-		@shirts = Shirt.all
-	end
+  def search
+
+      collar_index = bust_index = waist_index = arm_index = search_run_indexer = 0
+
+      while search_run_indexer < 30 do
+				set_sizes_ranges(collar_index, bust_index, waist_index, arm_index)
+        #@shirts = Shirt.show(shirt_params)
+				@shirts = Shirt.where(:size_collar => @collar_range, :size_bust => @bust_range, :size_waist => @waist_range, :size_arm => @arm_range)
+        search_run_indexer += 1
+        bust_index += 1
+        waist_index += 1
+        arm_index += 1
+        
+        if search_run_indexer == 10
+          collar_index = 1
+          bust_index = 0
+          waist_index = 0
+          arm_index = 0
+        end
+        
+        if search_run_indexer == 20
+          collar_index = 1
+          bust_index = 0
+          waist_index = 0
+          arm_index = 0
+          if params[:size_arm].to_i > 20
+            params[:size_arm] = 15
+            @shortsleeve = true
+          else
+            params[:size_arm] = 70
+            @longsleeve = true
+          end
+        end
+
+
+				#puts "index: " + search_run_indexer.to_s
+				#puts "results: " + @shirts.size_collar.to_s
+
+        if @shirts.size > 4
+          break
+        end
+
+      end #while index < 100
+      
+      #@sorted_shirts = @shirts.order("size_collar ASC").all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @shirts }
+    end
+  end
 
   # GET /shirts/1
   # GET /shirts/1.json
@@ -73,6 +121,45 @@ class ShirtsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def shirt_params
-      params.require(:shirt).permit(:code, :label, :description, :ean, :profile, :collar, :breast_pocket, :wristband, :color, :cloth, :price, :link, :affiliate, :picture, :size_collar, :size_bust, :size_waist, :size_body, :size_arm, :size_shoulder, :size_back, :size_wrist, :sleeve)
+      params.require(:shirt).permit(:code, :label, :description, :ean, :profile, :collar, :breast_pocket, :wristband, :color, :cloth, :price, :link, :affiliate, :picture, :size_collar, :size_bust, :size_waist, :size_body, :size_arm, :size_shoulder, :size_back, :size_wrist, :sleeve) if params[:shirt] 
     end
+
+		def set_sizes_ranges(collar_index, bust_index, waist_index, arm_index)
+			if params[:size_collar].to_i == 0
+				size_collar_max = 1000000
+				size_collar_min = 0
+			else
+				size_collar_max = params[:size_collar].to_i + collar_index + 1
+				size_collar_min = params[:size_collar].to_i - collar_index
+			end
+			@collar_range = size_collar_min...size_collar_max
+			
+			if params[:size_bust].to_i == 0
+				size_bust_max = 1000000
+				size_bust_min = 0
+			else
+				size_bust_max = params[:size_bust].to_i + bust_index + 1
+				size_bust_min = params[:size_bust].to_i
+			end
+			@bust_range = size_bust_min...size_bust_max
+
+			if params[:size_waist].to_i == 0
+				size_waist_max = 100000
+				size_waist_min = 0
+			else
+				size_waist_max = params[:size_waist].to_i + waist_index + 1
+				size_waist_min = params[:size_waist].to_i
+			end
+			@waist_range = size_waist_min...size_waist_max
+
+			if params[:size_arm].to_i == 0
+				size_arm_max = 100000
+				size_arm_min = 0
+			else
+				size_arm_max = params[:size_arm].to_i + arm_index + 1
+				size_arm_min = params[:size_arm].to_i - arm_index
+			end
+			@arm_range = size_arm_min...size_arm_max
+		end
+		
 end
--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,2 +1,7 @@
 class Shirt < ActiveRecord::Base
+
+
+#	where(:size_collar => size_collar_min...size_collar_max, :size_bust => size_bust_min...size_bust_max, :size_waist => size_waist_min...size_waist_max, :size_arm => size_arm_min...size_arm_max)
+
+
 end
